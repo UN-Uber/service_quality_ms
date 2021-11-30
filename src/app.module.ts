8c5c2dfc6dfaebc8b3f45dbddf571eeedcd42beb
Juan Camilo Villota Ballesteros <38733879+jcvillotab@users.noreply.github.com>
2021-11-30 08:22:48 -0500

    Fixing moongose async init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { DriverModule } from './Driver/driver.module';
-import { MONGO_CONNECTION } from './app.properties';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CalificationModule } from './Calification/calification.module';
 import { UserModule } from './User/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DriverModule, MongooseModule.forRoot(MONGO_CONNECTION), CalificationModule, UserModule],
+  imports: [ConfigModule.forRoot(), DriverModule, CalificationModule, UserModule,  MongooseModule.forRootAsync({
+      useFactory : () => ({
+        uri: `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
+      }),
+  })],
 })
 export class AppModule {}
