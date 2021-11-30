9e17bbe6182684a569f021fb98c0238df91cac1b
Juan Camilo Villota Ballesteros <38733879+jcvillotab@users.noreply.github.com>
2021-11-30 07:57:57 -0500

    Fixing env variables

--- src/app.properties.ts
+++ src/app.properties.ts
@@ -1,1 +1,5 @@
-export const MONGO_CONNECTION = "mongodb+srv://jcvillotab:uberclone@clusteruberclone.71c8z.mongodb.net/service_quality_db?retryWrites=true&w=majority"
+const DATABASE_NAME = process.env.DATABASE_NAME;
+const DATABASE_USER = process.env.DATABASE_USER;
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+const DATABASE_HOST = process.env.DATABASE_HOST;
+export const MONGO_CONNECTION = `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}/${DATABASE_NAME}?retryWrites=true&w=majority`
